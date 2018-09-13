8cdb2ee059530ee8749a79d944782d9e822ffb01
Aman <amansharma965@gmail.com>
2018-09-12 19:41:44 -0700

    minor chnages

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,22 +1,22 @@
 //array that holds all the 'friends' that have taken the survey
 //initialize starting with 'dummy' data
 var friendList = [
-    {
-      name: 'Amandeep Sharma',
-      profilePic: 'https://qph.fs.quoracdn.net/main-qimg-305be0c3291e837dcbbd0615616dbfbe',
-      scores: [
-        "4",
-        "5",
-        "5",
-        "2",
-        "4",
-        "5",
-        "4",
-        "4",
-        "3",
-        "4"
-      ]
-    }
-  ]
-  
-  module.exports = friendList;
+  {
+    name: 'Stone Cold',
+    profilePic: 'https://avatars3.githubusercontent.com/u/17751317?v=3&s=466',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
